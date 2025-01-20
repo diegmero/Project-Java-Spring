a33d618f4f4e2f40ade8a5db560bd635bc459c5f
diegmero <diegmero12@gmail.com>
2025-01-19 22:48:25 -0500

    primer comentario web Spring Java

---
+++ src/main/java/com/example/web/MiWebSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiWebSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiWebSpringApplication.class, args);
+	}
+
+}
